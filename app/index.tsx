cf8d5e84992773820891b8bcd89320796beafb24
Rithvik8001 <1017rithvik@gmail.com>
2025-03-25 15:01:42 -0400

    checbox and delete

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,58 @@
-import { Text, View, SafeAreaView, FlatList, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  FlatList,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import Checkbox from "expo-checkbox";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Index() {
   const todoItems = [
-    { id: 1, title: "This is First Todo!", isCompleted: false },
-    { id: 2, title: "This is Second Todo!", isCompleted: false },
-    { id: 3, title: "This is Third Todo!", isCompleted: false },
+    { id: 1, title: "Complete RN Todo list!", isCompleted: false },
+    { id: 2, title: "Study About Expo Router!", isCompleted: false },
+    { id: 3, title: "This is Third Todo!", isCompleted: true },
     { id: 4, title: "This is Fourth Todo!", isCompleted: false },
     { id: 5, title: "This is Fifth Todo!", isCompleted: false },
   ];
 
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <Text>Your Todos</Text>
+      {/* Header (Clean, Minimalist) */}
+      <View style={styles.header}>
+        <Text style={styles.title}>Todos</Text>
+        <Image
+          source={{ uri: "https://github.com/shadcn.png" }}
+          style={styles.avatar}
+        />
       </View>
+
+      {/* Todo List (Neo-Brutalist Style) */}
       <FlatList
-        style={styles.todoContainer}
+        contentContainerStyle={styles.todoContainer}
         data={todoItems}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
           <View style={styles.card}>
-            <Text style={styles.text}>{item.title}</Text>
+            <Checkbox style={styles.checkbox} value={item.isCompleted} />
+            <Text
+              style={[
+                styles.text,
+                item.isCompleted && { textDecorationLine: "line-through" },
+              ]}
+            >
+              {item.title}
+            </Text>
+            <TouchableOpacity
+              onPress={() => {
+                alert("Todo Deleted");
+              }}
+            >
+              <Ionicons name="trash" size={18} color={"red"} />
+            </TouchableOpacity>
           </View>
         )}
       />
@@ -31,30 +63,51 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fafafa", // Ultra-light background
-    paddingVertical: 20,
-    paddingHorizontal: 10,
+    backgroundColor: "#fafafa",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 15,
+    paddingTop: 20,
+  },
+  title: {
+    fontSize: 34,
+    fontWeight: "800",
+    color: "brown",
+  },
+  avatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+  },
+  todoContainer: {
+    padding: 15,
   },
   card: {
     backgroundColor: "#fff",
-    padding: 20,
+    padding: 14,
     marginVertical: 10,
     borderRadius: 8,
     borderWidth: 3,
     borderColor: "#000",
     shadowColor: "#000",
-    shadowOffset: { width: 6, height: 6 }, // Sharp offset shadow
+    shadowOffset: { width: 6, height: 6 },
     shadowOpacity: 1,
-    shadowRadius: 0,
+    shadowRadius: 1,
     elevation: 10, // For Android
+    flexDirection: "row",
+    alignContent: "center",
+    justifyContent: "space-between",
   },
   text: {
     fontSize: 16,
-    fontWeight: "bold",
+    fontWeight: "semibold",
     textTransform: "uppercase",
     color: "#000",
   },
-  todoContainer: {
-    padding: 20,
+  checkbox: {
+    marginRight: 15,
   },
 });
